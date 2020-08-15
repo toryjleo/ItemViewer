fa35acf9177f3886df1f59a88eef7d2dcfab7e43
Tory <toryjleo@gmail.com>
2020-08-15 15:30:01 -0400

    Created InpectibleObject script

---
+++ ItemViewer/Assets/Scripts/InspectableObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InspectableObject : MonoBehaviour
+{
+    public float rotationSpeed = 90f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        
+    }
+
+    public void RotateUp() 
+    {
+    
+    }
+
+    public void RotateLeft()
+    {
+        transform.RotateAround(transform.position, transform.up, Time.deltaTime * rotationSpeed);
+    }
+
+    void RotateRight() 
+    {
+        transform.RotateAround(transform.position, transform.up, -Time.deltaTime * rotationSpeed);
+    }
+}
